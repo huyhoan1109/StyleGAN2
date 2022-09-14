9fdabb5fc6ae3497e31c01f2e007c6e331420dfe
huyhoan1109 <80458315+huyhoan1109@users.noreply.github.com>
2022-09-15 05:24:16 +0700

    update

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 import math
+from torch.autograd import grad, Variable
 import torch
 
-def PathLengthRegulator(faked_image, latent, mean_path, w_decay):
+def PathLengthRegulator(
+    faked_image:torch.Tensor, 
+    latent:torch.Tensor, 
+    mean_path:float, 
+    w_decay:float):
     # faked_images => Generated noise y
     y = torch.randn(faked_image.shape) /  math.sqrt(
         faked_image.shape[2] * faked_image.shape[3]
@@ -9,16 +14,15 @@ def PathLengthRegulator(faked_image, latent, mean_path, w_decay):
     # output = g(w) * y
     output = (faked_image * y).sum()
     # gradient = Jw * y 
-    gradient = torch.autograd.grad(
+    gradient = grad(
         outputs=output, 
         inputs=latent, 
         allow_unused=True, 
         create_graph=True)[0]
     if gradient == None:
-        gradient = torch.randn(mean_path.shape)
+        raise AssertionError("Can't calculate gradient!")
     # Calculate penalty
     penalty = (((gradient - mean_path) ** 2).sum()).sqrt()
-
     # new a = a * (1-w_decay) + ||Jw * y - a||(2) * w_decay
     path_length = ((gradient ** 2).sum()).sqrt()
     new_mean_path = mean_path * (1-w_decay) + w_decay * path_length
